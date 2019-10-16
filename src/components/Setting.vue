41e4053ea7de3e1069f6512d6d4abb1a486ea693
flx <fulixiu@shinewonder.com>
2019-10-16 17:54:50 +0800

    Update Setting.vue

--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,9 +1,9 @@
 <template>
 <div id="setting-page">
     <div class="left-menu">
-        <div class="menu" @click="gotoPath('authority')">authority</div>
-        <div class="menu" @click="gotoPath('theme')">theme</div>
-        <div class="menu" @click="gotoPath('plugs')">plugs</div>
+        <div class="menu" :class="[activeMenu==='authority'?'select':'']" @click="gotoPath('authority')">authority</div>
+        <div class="menu" :class="[activeMenu==='theme'?'select':'']" @click="gotoPath('theme')">theme</div>
+        <div class="menu" :class="[activeMenu==='plugs'?'select':'']" @click="gotoPath('plugs')">plugs</div>
     </div>
     <div class="view">
         <router-view class="view-body"></router-view>
@@ -30,10 +30,15 @@
             height: 50px;
             line-height: 50px;
         }
-        .menu :hover{
+        .menu:hover{
             height: 50px;
             line-height: 50px;
-            background:#CCCCCC; 
+            background:#ffffff; 
+        }
+        .menu.select{
+            height: 50px;
+            line-height: 50px;
+            background:#cccccc; 
         }
     }
 }
@@ -44,11 +49,13 @@ export default {
     name: 'HelloWorld',
     data() {
         return {
+            activeMenu:"/",
             msg: 'Welcome to Your Vue.js App'
         }
     },
     methods: {
         gotoPath(path) {
+            this.activeMenu=path;
             this.$router.push({
                 path: '/setting/' + path
             });
